7805e1d1a1c647fb8d6ec75c38b484b54d7b3ac7
B-Mugnol <bruno_mugnol@hotmail.com>
2022-01-28 19:28:18 +0100

    changed error message

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:51:33 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/01/26 22:44:37 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/01/28 19:16:39 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int	arg_error(char *err_msg, char *description)
 	errno = EINVAL;
 	perror(err_msg);
 	ft_putstr_fd(description, 1);
-	ft_putendl_fd(" Correct usage:\t./fdf <filename>.fdf", 1);
+	ft_putendl_fd("\nCorrect usage example:\t./fdf mapname.fdf", 1);
 	return (0);
 }
