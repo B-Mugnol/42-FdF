2ee8637b439055fdd2f924f539b7e1bdf3128a64
B-Mugnol <bruno_mugnol@hotmail.com>
2022-02-10 19:16:12 +0100

    added hooks file on Makefile, included/updated prototypes in header

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:51:36 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/02/10 17:55:45 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/02/10 18:53:22 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,16 @@
 # include <string.h>
 # include <errno.h>
 
+// Screen size
 # define WINDOW_WIDTH	720
 # define WINDOW_HEIGHT	360
+
+// X11 used event codes and their masks
+# define X_KEY_PRESS_EVENT	02
+# define X_KEY_PRESS_MASK	1L<<0
+
+// Key values
+# define KEY_ESC	65307
 
 typedef struct s_image {
 	void	*mlx_img;
@@ -54,9 +62,12 @@ int		is_valid_data(t_data *data);
 
 // setup.c
 t_data	mount_mlx(char *filename);
-void	dismount_mlx(t_data *data);
+int		dismount_mlx(t_data *data);
 
 // render.c
 void	put_image_pixel(t_image *image, int x, int y, int color);
+
+// hooks.c
+int	key_press_hook(int keycode, t_data *data);
 
 #endif
