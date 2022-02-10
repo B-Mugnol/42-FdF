a19106728dec09f65543b3573a2026219e74541f
B-Mugnol <bruno_mugnol@hotmail.com>
2022-02-10 19:14:40 +0100

    is_valid_data now only performs one acton: answer its name

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 22:33:41 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/02/10 01:02:17 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/02/10 19:11:19 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int	is_valid_data(t_data *data)
 {
 	if (data->is_valid != 0)
 		return (1);
-	dismount_mlx(data);
 	return (0);
 }
 
