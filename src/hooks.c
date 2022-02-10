941baeeb4e2f4955d56fbb3bb137f1b3e68c8ca3
B-Mugnol <bruno_mugnol@hotmail.com>
2022-02-10 19:13:27 +0100

    added a key_press_hook function

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,20 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 00:19:54 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/02/10 00:19:55 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/02/10 19:11:45 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fdf.h"
+
+int	key_press_hook(int keycode, t_data *data)
+{
+	printf("%i\n", keycode);
+	if (keycode == KEY_ESC)
+	{
+		mlx_loop_end(data->mlx);
+		return (1);
+	}
+		// return (dismount_mlx(data));
+	return (0);
+}
