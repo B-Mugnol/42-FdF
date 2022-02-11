016e8e4461e52bd98fe04b1ade74a02f6bebbf59
B-Mugnol <bruno_mugnol@hotmail.com>
2022-02-11 22:43:37 +0100

    removed commented line

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 00:19:54 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/02/10 19:11:45 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/02/11 22:43:15 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,5 @@ int	key_press_hook(int keycode, t_data *data)
 		mlx_loop_end(data->mlx);
 		return (1);
 	}
-		// return (dismount_mlx(data));
 	return (0);
 }
