9a19e130d06a6d61ee1b031f0c40fc8c24b9fb82
B-Mugnol <bruno_mugnol@hotmail.com>
2022-02-26 22:06:04 +0100

    changed status messages and set map.window_coverage to 80%

--- src/setup.c
+++ src/setup.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 17:22:26 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/02/25 22:53:58 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/02/26 21:59:48 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,14 @@ t_data	mount_mlx(char *filename)
 
 int	dismount_mlx(t_data *data)
 {
+	ft_putstr_fd("Clearing Minilibx assets... ", 1);
 	mlx_clear_window(data->mlx, data->win);
 	mlx_loop_end(data->mlx);
 	mlx_destroy_image(data->mlx, data->image.mlx_img);
 	mlx_destroy_window(data->mlx, data->win);
 	mlx_destroy_display(data->mlx);
 	free(data->mlx);
-	ft_putendl_fd("Program terminated succesfully.", 1);
+	ft_putendl_fd("Minilibx assets cleared.", 1);
 	return (1);
 }
 
@@ -68,6 +69,7 @@ t_map	map_init(void)
 	map.projection = NULL;
 	map.z_scale = 1;
 	map.scale_factor = 1;
+	map.window_coverage = .8;
 	map.lines = 0;
 	map.columns = 0;
 	r.x_rot = 54.74 * DEG_TO_RAD;
@@ -89,6 +91,7 @@ int	map_clear(t_map *map)
 	t_rot	r;
 	t_point	pt;
 
+	ft_putstr_fd("Clearing map... " , 1);
 	if (map->str_map)
 		ft_free_matrix((void *)(&map->str_map), map->lines);
 	if (map->base_map)
@@ -110,4 +113,5 @@ int	map_clear(t_map *map)
 	pt.x = -WINDOW_WIDTH;
 	pt.y = -WINDOW_HEIGHT;
 	map->max = pt;
+	ft_putendl_fd("Map cleared." , 1);
 }
