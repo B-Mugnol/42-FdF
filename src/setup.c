9c44f69571cf7496a5579119955bb653488aadd4
B-Mugnol <bruno_mugnol@hotmail.com>
2022-02-26 18:45:24 +0100

    added map_init and map_clear functions

--- src/setup.c
+++ src/setup.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 17:22:26 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/02/10 19:07:48 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/02/25 22:53:58 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ int	dismount_mlx(t_data *data)
 	mlx_destroy_window(data->mlx, data->win);
 	mlx_destroy_display(data->mlx);
 	free(data->mlx);
+	ft_putendl_fd("Program terminated succesfully.", 1);
 	return (1);
 }
 
@@ -54,4 +55,59 @@ static void	draw_background(t_image *image)
 	i = 0;
 	while (i < (size_t)(WINDOW_HEIGHT) * image->line_size)
 		*(pixel + i++) = 0xFF;
+}
+
+t_map	map_init(void)
+{
+	t_map	map;
+	t_rot	r;
+	t_point	pt;
+
+	map.str_map = NULL;
+	map.base_map = NULL;
+	map.projection = NULL;
+	map.z_scale = 1;
+	map.scale_factor = 1;
+	map.lines = 0;
+	map.columns = 0;
+	r.x_rot = 54.74 * DEG_TO_RAD;
+	r.y_rot = 0 * DEG_TO_RAD;
+	r.z_rot = 45 * DEG_TO_RAD;
+	map.rotation = r;
+	pt.z = 0;
+	pt.x = WINDOW_WIDTH;
+	pt.y = WINDOW_HEIGHT;
+	map.min = pt;
+	pt.x = -WINDOW_WIDTH;
+	pt.y = -WINDOW_HEIGHT;
+	map.max = pt;
+	return (map);
+}
+
+int	map_clear(t_map *map)
+{
+	t_rot	r;
+	t_point	pt;
+
+	if (map->str_map)
+		ft_free_matrix((void *)(&map->str_map), map->lines);
+	if (map->base_map)
+		ft_free_matrix((void *)(&map->base_map), map->lines);
+	if (map->projection)
+		ft_free_matrix((void *)(&map->projection), map->lines);
+	map->z_scale = 1;
+	map->scale_factor = 1;
+	map->lines = 0;
+	map->columns = 0;
+	r.x_rot = 54.74 * DEG_TO_RAD;
+	r.y_rot = 0 * DEG_TO_RAD;
+	r.z_rot = 45 * DEG_TO_RAD;
+	map->rotation = r;
+	pt.z = 0;
+	pt.x = WINDOW_WIDTH;
+	pt.y = WINDOW_HEIGHT;
+	map->min = pt;
+	pt.x = -WINDOW_WIDTH;
+	pt.y = -WINDOW_HEIGHT;
+	map->max = pt;
 }
